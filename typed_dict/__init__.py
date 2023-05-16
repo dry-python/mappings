247c2cf47e2ae727153adcf0697d38960906909c
gram <git@orsinium.dev>
2023-05-16 15:04:30 +0200

    simplify plugin path

--- typed_dict/__init__.py
+++ typed_dict/__init__.py
@@ -1,6 +1,8 @@
 """A library to generate TypedDict from different types, with mypy support."""
 from ._from import from_dataclass
+from ._mypy_plugin import plugin
 
 __all__ = [  # noqa: WPS410
     'from_dataclass',
+    'plugin',
 ]
